2d0ae74ea03d3f6a52b5a5655cd6e8c4095e31a8
Alf-André Walla <fwsgonzo@hotmail.com>
2024-07-01 22:48:27 +0200

    Initial commit

---
+++ src/riscv.hpp
@@ -0,0 +1,53 @@
+#pragma once
+
+#include <godot_cpp/classes/control.hpp>
+#include <godot_cpp/classes/global_constants.hpp>
+#include <godot_cpp/classes/viewport.hpp>
+
+#include <godot_cpp/core/binder_common.hpp>
+#include <libriscv/machine.hpp>
+
+using namespace godot;
+#define RISCV_ARCH riscv::RISCV64
+using gaddr_t = riscv::address_type<RISCV_ARCH>;
+using machine_t = riscv::Machine<RISCV_ARCH>;
+
+class RiscvEmulator : public Control
+{
+	GDCLASS(RiscvEmulator, Control);
+
+protected:
+	static void _bind_methods();
+
+	String _to_string() const;
+
+public:
+	static constexpr uint64_t MAX_INSTRUCTIONS = 16'000'000'000ULL;
+
+	RiscvEmulator();
+	~RiscvEmulator();
+
+	auto& machine() { return *m_machine; }
+	const auto& machine() const { return *m_machine; }
+
+	const String& name();
+
+	// Functions.
+	void load(const PackedByteArray& buffer, const TypedArray<String>& arguments);
+	void exec();
+	GDExtensionInt call(String function);
+
+	void print(std::string_view text);
+	gaddr_t address_of(std::string_view name) const;
+
+private:
+	void handle_exception(gaddr_t);
+	void handle_timeout(gaddr_t);
+
+	machine_t* m_machine = nullptr;
+	std::vector<uint8_t> m_binary;
+
+	bool m_last_newline = false;
+	unsigned m_budget_overruns = 0;
+	String m_name;
+};
